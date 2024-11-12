8e177ca261b50f7ac3dd8d4f25fedeb2a121370c
Eduardo Valdes <eduardo@Eduardos-MacBook-Air.local>
2024-11-11 23:56:43 -0600

    fix calendar

--- src/app/citasdev/page.tsx
+++ src/app/citasdev/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import {  dateFnsLocalizer } from "react-big-calendar";
+import Calendar from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
