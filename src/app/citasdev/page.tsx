74b37aee4d5813f0b2a6f8fd7851a06c76cabd53
Eduardo Valdes <eduardo@Eduardos-MacBook-Air.local>
2024-11-11 23:59:12 -0600

    start of week

--- src/app/citasdev/page.tsx
+++ src/app/citasdev/page.tsx
@@ -5,7 +5,7 @@ import {  dateFnsLocalizer } from "react-big-calendar";
 import Calendar from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
+import { startOfWeek } from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import es from "date-fns/locale/es";
 import addMonths from "date-fns/addMonths";
