c83f199518632dd4ec5aed52e89ec767cc4466a2
adrianmorfin10 <adri_mo95@hotmail.com>
2024-12-13 21:51:30 -0600

    selector emp carrito

--- src/app/components/ListaDeElementos.tsx
+++ src/app/components/ListaDeElementos.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import { AppContext } from './AppContext';
 import ProductoService from '../services/ProductoService';
 const productoObject = new ProductoService();
+
 type SectionType = "Por Cobrar" | "Productos" | "Membresías";
 
 interface Item {
@@ -18,39 +19,22 @@ interface ListaDeElementosProps {
 }
 
 const ListaDeElementos: React.FC<ListaDeElementosProps> = ({ section, onAddToCart }) => {
-  const [ items, setItems] = React.useState<Record<SectionType, Item[]>>({
-    // "Venta Rápida": [
-    //   { id: 1, nombre: "Corte de Cabello", precio: 100 },
-    //   { id: 2, nombre: "Peinado", precio: 80 },
-    // ],
-    "Por Cobrar": [
-      // { id: 3, nombre: "Corte de Cabello - Juan", precio: 100 },
-      // { id: 4, nombre: "Coloración - María", precio: 150 },
-    ],
-    "Productos": [
-      // { id: 5, nombre: "Producto A", precio: 50 },
-      // { id: 6, nombre: "Producto B", precio: 120 },
-    ],
-    "Membresías": [
-      // { id: 7, nombre: "Black", precio: 300 },
-      // { id: 8, nombre: "White", precio: 200 },
-    ],
-  })
- 
-  const [ state, dispatchState ]= React.useContext(AppContext);
+  const [items, setItems] = React.useState<Record<SectionType, Item[]>>({
+    "Por Cobrar": [],
+    "Productos": [],
+    "Membresías": [],
+  });
+
+  const [state, dispatchState] = React.useContext(AppContext);
 
   React.useEffect(() => {
- 
-    
-      productoObject.getProductos(state.sucursal ? { local_id: state.sucursal.id } : false).then(response=>{
-        const productos = response.map((item: any)=>{
-          const { precio_productos, id, nombre, } = item;
-          return { id, nombre, precio: precio_productos.length ? precio_productos[0].precio : 0  }
-        });
-        setItems({ ...items, ["Productos"]: productos })
-      })
-    
-  
+    productoObject.getProductos(state.sucursal ? { local_id: state.sucursal.id } : false).then(response => {
+      const productos = response.map((item: any) => {
+        const { precio_productos, id, nombre } = item;
+        return { id, nombre, precio: precio_productos.length ? precio_productos[0].precio : 0 };
+      });
+      setItems({ ...items, ["Productos"]: productos });
+    });
   }, [state.sucursal]);
 
   return (
@@ -60,7 +44,7 @@ const ListaDeElementos: React.FC<ListaDeElementosProps> = ({ section, onAddToCar
         {(items[section] || []).map((item) => (
           <li
             key={item.id}
-            onClick={() => onAddToCart(item)}
+            onClick={() => onAddToCart(item)}  // Aquí se agrega el producto al carrito
             className="p-3 bg-white rounded cursor-pointer hover:bg-gray-100 flex justify-between shadow"
           >
             <span>{item.nombre}</span>
