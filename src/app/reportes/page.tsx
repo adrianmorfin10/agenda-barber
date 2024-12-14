cf09b205fcb01e86a92d91cfc96b86f518ab3d15
adrianmorfin10 <adri_mo95@hotmail.com>
2024-12-14 13:07:40 -0600

    reportes fixeds

--- src/app/reportes/page.tsx
+++ src/app/reportes/page.tsx
@@ -2,18 +2,23 @@
 
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import RepNavBar from '../components/RepNavBar'; // Importa el componente RepNavBar
-import Reportes from './reportes'; // Asegúrate de que la ruta sea correcta
+import Reportess from './Reportes'; // Asegúrate de que la ruta sea correcta
 
 const Page: React.FC = () => {
+  // Define la función onDateChange
+  const handleDateChange = (date: Date) => {
+    console.log('Fecha seleccionada:', date);
+  };
+
   return (
     <main className="min-h-screen bg-white p-4">
       {/* Barra de navegación */}
       <RepNavBar /> {/* Aquí se usa el componente RepNavBar */}
 
       {/* Contenido de la página */}
-      <Reportes />
+      <Reportess onDateChange={handleDateChange} /> {/* Pasamos onDateChange a Reportess */}
     </main>
   );
 };
