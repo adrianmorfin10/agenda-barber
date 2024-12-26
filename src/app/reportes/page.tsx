813c7da84c38d768209b1870021ab97a34ea6520
Eduardo Valdes <eduardo@Eduardos-MacBook-Air.local>
2024-12-26 16:56:43 -0600

    fix show report

--- src/app/reportes/page.tsx
+++ src/app/reportes/page.tsx
@@ -6,6 +6,7 @@ import React, { useState } from 'react';
 import RepNavBar from '../components/RepNavBar'; // Importa el componente RepNavBar
 import Reportess from './Reportes'; // Asegúrate de que la ruta sea correcta
 import { AppContext } from '../components/AppContext';
+import { getRole } from '../Utils';
 
 const Page: React.FC = () => {
   const [state, dispatchState] = React.useContext(AppContext);
@@ -14,7 +15,7 @@ const Page: React.FC = () => {
     console.log('Fecha seleccionada:', date);
   };
 
-  if(state.user.barberos) return null;
+  if(getRole(state.user) !== "admin") return null;
   return (
     <main className="min-h-screen bg-white p-4">
       {/* Barra de navegación */}
