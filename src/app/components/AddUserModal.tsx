eeca005be4efacf3ce1269a1d03b49d332f24489
adrianmorfin10 <adri_mo95@hotmail.com>
2025-01-05 16:56:00 -0600

    empklss

--- src/app/components/AddUserModal.tsx
+++ src/app/components/AddUserModal.tsx
@@ -80,6 +80,12 @@ const AddUserModal: React.FC<AddUserModalProps> = ({ isModalOpen, setIsModalOpen
   };
 
   const handleAddCliente = () => {
+    if (!nuevoCliente.creado_por) { // Validación de empleado seleccionado
+      setErrorMessage('Debe seleccionar un empleado.');
+      setErrorModalOpen(true);
+      return;
+    }
+  
     const clientService = new ClientService();
     const formData = new FormData();
     formData.append('nombre', nuevoCliente.nombre);
@@ -94,8 +100,7 @@ const AddUserModal: React.FC<AddUserModalProps> = ({ isModalOpen, setIsModalOpen
     if (file) {
       formData.append('foto', file);
     }
-    
-
+  
     clientService.createClient(formData)
       .then((response) => {
         setNuevoCliente({
@@ -120,6 +125,7 @@ const AddUserModal: React.FC<AddUserModalProps> = ({ isModalOpen, setIsModalOpen
         console.error('Error al añadir cliente:', error);
       });
   };
+  
 
   const handleDescuentoChange = (increment: boolean) => {
     setNuevoCliente((prev) => ({
@@ -189,19 +195,20 @@ const AddUserModal: React.FC<AddUserModalProps> = ({ isModalOpen, setIsModalOpen
             maxLength={50}
             required
           />
-          <select
-            name="employee"
-            value={nuevoCliente.creado_por}
-            onChange={(e)=>{ setNuevoCliente({ ...nuevoCliente, creado_por: parseInt(e.target.value) })}}
-            className={`border p-2 mb-4 w-full rounded-lg text-black placeholder-gray`}
-          >
-            <option value="">Selecciona un empleado</option>
-            {empleados.map((emp:Empleado) => (
-              <option key={emp.id} value={emp.id}>
-                {`${emp.nombre} ${emp.apellido} `} 
-              </option>
-            ))}
-          </select>
+         <select
+  name="creado_por"
+  value={nuevoCliente.creado_por}
+  onChange={(e) => setNuevoCliente({ ...nuevoCliente, creado_por: parseInt(e.target.value) })}
+  className="border p-2 mb-2 w-full rounded-lg text-black placeholder-gray"
+  required
+>
+  <option value="">Seleccione un empleado</option>
+  {empleados.map((empleado) => (
+    <option key={empleado.id} value={empleado.id}>
+      {empleado.nombre} {empleado.apellido}
+    </option>
+  ))}
+</select>
           <input
             type="email"
             name="email"
