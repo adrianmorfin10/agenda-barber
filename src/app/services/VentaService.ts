3c2ada947347e2060ed2f0d788a7054dbe1beb4f
adrianmorfin10 <adri_mo95@hotmail.com>
2025-02-20 12:12:39 -0600

    filtro

--- src/app/services/VentaService.ts
+++ src/app/services/VentaService.ts
@@ -2,40 +2,42 @@ import axios from "axios";
 import HttpService from "./HttpService";
 
 class VentaService extends HttpService {
+  constructor() {
+    super();
+  }
 
-    constructor() {
-        super();
-    }
+  async getAll(local_id: number, periodo: string, currentDate: string) {
+    console.log("current date", currentDate);
+    const response = await axios.get(
+      `${this.baseUrl}/venta?local_id=${local_id}&periodo=${periodo}&current_date=${currentDate}`
+    );
+    return response.data;
+  }
 
-    async getAll(local_id:number, periodo: string, currentDate:string){
-        console.log("current date", currentDate)
-        const response = await axios.get(`${this.baseUrl}/venta?local_id=${local_id}&periodo=${periodo}&current_date=${currentDate}`);
-        return response.data;
-    }
-    
-    async getCitasPorCobrarByLocal(local_id: any) {
-        const response = await axios.get(`${this.baseUrl}/venta/${local_id}/por-cobrar`);
-        return response.data;
-    }
+  async getCitasPorCobrarByLocal(local_id: any) {
+    const response = await axios.get(`${this.baseUrl}/venta/${local_id}/por-cobrar`);
+    return response.data;
+  }
 
-    async getMembresiasPorCobrarByLocal(local_id: any) {
-        const response = await axios.get(`${this.baseUrl}/venta/${local_id}/membresias`);
-        return response.data;
-    }
+  async getMembresiasPorCobrarByLocal(local_id: any) {
+    const response = await axios.get(`${this.baseUrl}/venta/${local_id}/membresias`);
+    return response.data;
+  }
 
+  async deleteVenta(id: number) {
+    const response = await axios.post(`${this.baseUrl}/venta/${id}/delete`);
+    return response.data;
+  }
 
-    async deleteVenta(id:number){
-        const response = await axios.post(`${this.baseUrl}/venta/${id}/delete`);
-        return response.data;
-    }
+  async checkout(carrito: any) {
+    const response = await axios.post(`${this.baseUrl}/venta/create`, carrito);
+    return response.data;
+  }
 
-    async checkout(carrito:any){
-
-        const response = await axios.post(`${this.baseUrl}/venta/create`, carrito);
-        return response.data;
-         
-    }
-    
-    
+  async getEmpleados(local_id: number) {
+    const response = await axios.get(`${this.baseUrl}/barbero?local_id=${local_id}`);
+    return response.data;
+  }
 }
+
 export default VentaService;
--- src/app/ventas-history/page.tsx
+++ src/app/ventas-history/page.tsx
@@ -1,107 +1,122 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SidebarVentas from "../components/SidebarVentas";
 import { AppContext } from "../components/AppContext";
 import VentaService from "../services/VentaService";
 import moment from "moment";
+
 const ventasObject = new VentaService();
 
 // Define el tipo de las secciones
 type SectionType = "Venta Rápida" | "Por Cobrar" | "Productos" | "Membresías" | "Ventas Realizadas";
 
-const getName = (venta:any)=>{
-  if(venta.venta_producto)
-    return venta.venta_producto.producto.nombre;
-  if(venta.venta_membresia)
-    return venta.venta_membresia.membresia.nombre;
-  if(venta.venta_servicio)
-    return venta.venta_servicio.servicio.nombre;
-  return `cita`
-}
+const getName = (venta: any) => {
+  if (venta.venta_producto) return venta.venta_producto.producto.nombre;
+  if (venta.venta_membresia) return venta.venta_membresia.membresia.nombre;
+  if (venta.venta_servicio) return venta.venta_servicio.servicio.nombre;
+  return `cita`;
+};
 
 const VentasHistory: React.FC = () => {
   const [selectedSection, setSelectedSection] = useState<SectionType | null>("Ventas Realizadas");
-  const [filterType, setFilterType] = useState<'dia' | 'semana' | 'mes' | 'year'>('dia');
-  const [sales, setSales] = useState<{id:number, hora: string; fecha: string, empleado: string, producto: string, precio: string, cliente: string}[]>([]);
+  const [filterType, setFilterType] = useState<"dia" | "semana" | "mes" | "year">("dia");
+  const [sales, setSales] = useState<
+    { id: number; hora: string; fecha: string; empleado: string; producto: string; precio: string; cliente: string }[]
+  >([]);
   const [currentDate, setCurrentDate] = useState(new Date());
   const [state, dispatchState] = React.useContext(AppContext);
+  const [selectedEmployee, setSelectedEmployee] = useState<string | null>(null);
+  const [employees, setEmployees] = useState<string[]>([]);
 
-  React.useEffect(() => {
-    if(!state.sucursal)
-      return;
-    getSales();
-  }, [state.sucursal, filterType, currentDate]);
+  useEffect(() => {
+    if (!state.sucursal) return;
 
-  const getSales = ()=>{
-    ventasObject.getAll(state.sucursal.id, filterType, moment(currentDate).format("YYYY-MM-DD hh:mm")).then((data:any)=>{
- 
-      const salesHsitory = data.map((item:any)=>{
-        
-        const ventaDate = moment(item.fecha).local();
-      
-        return{
-          id: item.id,
-          fecha: ventaDate.format("YYYY-MM-DD"),
-          hora: ventaDate.format("HH:mm"),
-          empleado: item.barbero.usuario.nombre,
-          producto: getName(item),
-          precio: `$${item.total}`,
-          cliente: item.carrito_compra.cliente?.usuario?.nombre || "Cliente no encontrado"
-        }
-        
+    // Obtener la lista de empleados
+    ventasObject
+      .getEmpleados(state.sucursal.id)
+      .then((data: any) => {
+        const employeeNames = data.map((employee: any) => employee.usuario.nombre);
+        setEmployees(employeeNames);
+      })
+      .catch((e: any) => {
+        console.error("Error al obtener empleados:", e);
       });
-      
-      setSales(salesHsitory);
-      
-    }).catch((e:any)=>{
-      
-    })
-  }
-  const changeDate = (_filterType:string, operation: 'add' | 'less'): Date=>{
+
+    // Obtener las ventas
+    getSales();
+  }, [state.sucursal, filterType, currentDate, selectedEmployee]);
+
+  const getSales = () => {
+    ventasObject
+      .getAll(state.sucursal.id, filterType, moment(currentDate).format("YYYY-MM-DD hh:mm"))
+      .then((data: any) => {
+        const salesHistory = data
+          .filter((item: any) => !selectedEmployee || item.barbero.usuario.nombre === selectedEmployee)
+          .map((item: any) => {
+            const ventaDate = moment(item.fecha).local();
+            return {
+              id: item.id,
+              fecha: ventaDate.format("YYYY-MM-DD"),
+              hora: ventaDate.format("HH:mm"),
+              empleado: item.barbero.usuario.nombre,
+              producto: getName(item),
+              precio: `$${item.total}`,
+              cliente: item.carrito_compra.cliente?.usuario?.nombre || "Cliente no encontrado",
+            };
+          });
+
+        setSales(salesHistory);
+      })
+      .catch((e: any) => {
+        console.error("Error al obtener ventas:", e);
+      });
+  };
+
+  const changeDate = (_filterType: string, operation: "add" | "less"): Date => {
     const newDate = moment(currentDate).utcOffset(0, false);
-    const value = operation === 'add' ? 1 : -1 ;
+    const value = operation === "add" ? 1 : -1;
     switch (filterType) {
-      case 'dia':
-        newDate.add(value, 'day')
+      case "dia":
+        newDate.add(value, "day");
         break;
-      case 'semana':
-        newDate.add(value, 'week')
+      case "semana":
+        newDate.add(value, "week");
         break;
-      case 'mes':
-        newDate.add(value, 'month')
+      case "mes":
+        newDate.add(value, "month");
         break;
-      case 'year':
-        newDate.add(value, 'year')
+      case "year":
+        newDate.add(value, "year");
         break;
     }
-    return newDate.toDate()
-  }
+    return newDate.toDate();
+  };
+
   const handlePrev = () => {
-    setCurrentDate(changeDate(filterType, 'less'));
+    setCurrentDate(changeDate(filterType, "less"));
   };
 
   const handleNext = () => {
-    setCurrentDate(changeDate(filterType, 'add'));
+    setCurrentDate(changeDate(filterType, "add"));
   };
+
   const totalVentas = sales.length;
-const totalPrecio = sales.reduce((sum, sale) => sum + parseFloat(sale.precio.replace('$', '')), 0)
+  const totalPrecio = sales.reduce((sum, sale) => sum + parseFloat(sale.precio.replace("$", "")), 0);
 
   const formatDate = () => {
-    if (filterType === 'dia') return currentDate.toLocaleDateString();
-    if (filterType === 'semana') {
+    if (filterType === "dia") return currentDate.toLocaleDateString();
+    if (filterType === "semana") {
       const startOfWeek = new Date(currentDate);
-      // Ajustar para que el inicio de la semana sea el lunes
       startOfWeek.setDate(currentDate.getDate() - (currentDate.getDay() === 0 ? 6 : currentDate.getDay() - 1));
       const endOfWeek = new Date(startOfWeek);
       endOfWeek.setDate(startOfWeek.getDate() + 6);
       return `${startOfWeek.toLocaleDateString()} - ${endOfWeek.toLocaleDateString()}`;
     }
-    if (filterType === 'mes') return currentDate.toLocaleDateString('default', { year: 'numeric', month: 'long' });
-    if (filterType === 'year') return currentDate.getFullYear();
+    if (filterType === "mes") return currentDate.toLocaleDateString("default", { year: "numeric", month: "long" });
+    if (filterType === "year") return currentDate.getFullYear();
   };
 
-  
   return (
     <div className="flex min-h-screen bg-gray-100 text-black">
       {/* Sidebar */}
@@ -114,44 +129,61 @@ const totalPrecio = sales.reduce((sum, sale) => sum + parseFloat(sale.precio.rep
         {/* Filtro */}
         <div className="mb-4">
           <div className="flex flex-wrap gap-2 mb-4">
-            {['dia', 'semana', 'mes', 'year'].map((type) => (
+            {["dia", "semana", "mes", "year"].map((type) => (
               <button
                 key={type}
                 className={`p-2 rounded text-sm font-light ${
-                  filterType === type ? 'bg-black text-white' : 'bg-gray-200 text-black hover:bg-gray-300'
+                  filterType === type ? "bg-black text-white" : "bg-gray-200 text-black hover:bg-gray-300"
                 }`}
-                onClick={() =>{
-                  setCurrentDate(new Date())
-                  setFilterType(type as 'dia' | 'semana' | 'mes' | 'year')
-                } }
+                onClick={() => {
+                  setCurrentDate(new Date());
+                  setFilterType(type as "dia" | "semana" | "mes" | "year");
+                }}
               >
                 {type.charAt(0).toUpperCase() + type.slice(1)}
               </button>
             ))}
           </div>
-          
-          
-            <div className="flex items-center justify-between mb-4">
-              <button className="p-2 bg-black rounded text-white hover:bg-gray-400" onClick={handlePrev}>
-                Anterior
-              </button>
-              <span className="text-xs font-bold text-black">{formatDate()}</span>
-              <button className="p-2 bg-black rounded text-white hover:bg-gray-400" onClick={handleNext}>
-                Siguiente
-              </button>
-            </div>
-          
-          
+
+          <div className="flex items-center justify-between mb-4">
+            <button className="p-2 bg-black rounded text-white hover:bg-gray-400" onClick={handlePrev}>
+              Anterior
+            </button>
+            <span className="text-xs font-bold text-black">{formatDate()}</span>
+            <button className="p-2 bg-black rounded text-white hover:bg-gray-400" onClick={handleNext}>
+              Siguiente
+            </button>
+          </div>
+
+          {/* Selector de empleados */}
+          <div className="mb-4">
+            <label htmlFor="employee" className="block text-sm font-medium text-gray-700">
+              Filtrar por empleado:
+            </label>
+            <select
+              id="employee"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm"
+              value={selectedEmployee || ""}
+              onChange={(e) => setSelectedEmployee(e.target.value || null)}
+            >
+              <option value="">Todos los empleados</option>
+              {employees.map((employee) => (
+                <option key={employee} value={employee}>
+                  {employee}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
 
-<div className="flex justify-between items-center mt-4">
-  <span className="text-sm font-medium">Total de ventas: {totalVentas}</span>
-  <span className="text-sm font-medium">Total: ${totalPrecio.toFixed(2)}</span>
-</div>
+        <div className="flex justify-between items-center mt-4">
+          <span className="text-sm font-medium">Total de ventas: {totalVentas}</span>
+          <span className="text-sm font-medium">Total: ${totalPrecio.toFixed(2)}</span>
+        </div>
 
         {/* Lista de ventas */}
         <div className="space-y-4">
-          {sales.map((sale:any) => (
+          {sales.map((sale: any) => (
             <div
               key={`sale-${sale.id}`}
               className="p-4 border rounded bg-gray-50 shadow-sm flex justify-between items-center"
@@ -164,22 +196,24 @@ const totalPrecio = sales.reduce((sum, sale) => sum + parseFloat(sale.precio.rep
               </div>
               <div className="p-2 flex flex-row items-center gap-2.5">
                 <span className="font-bold text-black">{sale.precio}</span>
-                {
-                  state.user?.rol === "admin" &&
-                  <button onClick={()=>{ 
-                    if(confirm('Esta seguro de eliminar esta venta ?'))
-                      ventasObject.deleteVenta(sale.id).then(()=>getSales()).catch(()=>alert('Ha ocurrido un error al elimina la venta'))
-                  }} className="border border-red-400 text-red-400 px-4 py-2 rounded text-sm md:text-base">
+                {state.user?.rol === "admin" && (
+                  <button
+                    onClick={() => {
+                      if (confirm("¿Está seguro de eliminar esta venta?"))
+                        ventasObject
+                          .deleteVenta(sale.id)
+                          .then(() => getSales())
+                          .catch(() => alert("Ha ocurrido un error al eliminar la venta"));
+                    }}
+                    className="border border-red-400 text-red-400 px-4 py-2 rounded text-sm md:text-base"
+                  >
                     Borrar
                   </button>
-                }
+                )}
               </div>
-              
             </div>
           ))}
-          {sales.length === 0 && (
-            <p className="text-gray-500 text-center">No hay ventas para este período.</p>
-          )}
+          {sales.length === 0 && <p className="text-gray-500 text-center">No hay ventas para este período.</p>}
         </div>
       </div>
     </div>
